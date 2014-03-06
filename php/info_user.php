89f4fbb6be5d951b702fcbc6c7a12262db3e1416
Thibault D'Archivio <thibault.darchivio@free.fr>
2014-03-06 16:40:54 +0100

    Correction de la requête de récupération des données d'un compte utilisateur

--- php/info_user.php
+++ php/info_user.php
@@ -5,17 +5,18 @@ if(isset($_POST['id'])) {
 	try {
 	    $bdd = new PDO('mysql:host=localhost;dbname=wwyd', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 	   
-	    $query = $bdd->prepare('SELECT * FROM user WHERE id = :id');
-	    $query->bindValue(':nb', $nb, PDO::PARAM_INT);
+	    $query = $bdd->prepare('SELECT * FROM user WHERE id = :nb');
+	    $query->bindValue(':nb', $_POST['id'], PDO::PARAM_INT);
 		$query->execute();
 		
 		$result = array();
 		$i = 0;
 		
-		while($data = $query->fetch()){
-			array_push($result, $data[$i]);
-			$i++;
-		}	
+		$result = $query->fetch();
+		
+		if (empty($result))
+			$result = array('error' => array('title' => 'Erreur', 'msg' => 'Aucun user trouvé'));
+		
 	} catch(Exception $e) {
 		$result = array('error' => array('title' => 'Erreur', 'msg' => 'Erreur base de données'));
 	}
