fb8ccd8d8d777076a83ab005b1940e82cdce3437
Thibault D'Archivio <thibault.darchivio@free.fr>
2014-03-06 16:16:52 +0100

    Correction de la requête de recherche d'utilisateur
    NoTest ftw

--- php/search_user.php
+++ php/search_user.php
@@ -5,10 +5,10 @@ if(isset($_POST['login'])) {
 	try {
 	    $bdd = new PDO('mysql:host=localhost;dbname=wwyd', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 	   
-	    $ch = mysql_real_escape_string($_POST['login']);
-	    $query = $bdd->prepare('SELECT login, id FROM user WHERE id LIKE "%'.$ch.'%"');
-	    $query->bindValue(':nb', $nb, PDO::PARAM_INT);
-		$query->execute();
+	    $query = $bdd->prepare('SELECT login, id FROM user WHERE login LIKE ?');
+		
+		$login = mysql_real_escape_string($_POST['login']);
+		$query->execute(array($login."%"));
 		
 		$result = array();
 		$i = 0;
