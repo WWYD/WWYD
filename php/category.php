cee88cedb71581531a3388ad73c90ecbf0a59ef7
Guiks2 <guillaume.delapre@gmail.com>
2014-03-07 14:46:40 +0100

    Ajout catégorie "toutes" dans le menu
    Ajout dans category.php

--- php/category.php
+++ php/category.php
@@ -1,4 +1,5 @@
 <?php
+	echo '<tr><td><a href="search.php?id=0" >Toutes</a></td></tr>';
 	$bdd = new PDO('mysql:host=localhost;dbname=wwyd', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 	$query = $bdd->query('SELECT name, id FROM category');
 	while($data = $query->fetch()){
